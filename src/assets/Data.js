ae1c1c803b14e0c93b48f9ffa4f9832292d18620
Jose-Hash-Dev <yusif.hashimov@outlook.com>
2021-08-04 21:00:42 +0300

    ProductList

---
+++ src/assets/Data.js
@@ -0,0 +1,30 @@
+import book1 from './Images/book1.jpg'
+import book2 from './Images/book2.jpg'
+import book3 from './Images/book3.jpg'
+
+const data =
+    [
+      {
+        id: 1,
+        image: book1,
+        price: '20',
+        title: 'The Da Vinci Code',
+        description: 'While in Paris, Harvard symbologist Robert Langdon is awakened by a phone call in the dead of the night. The elderly curator of the Louvre has been murdered inside the museum, his body covered in baffling symbols.'
+      },
+      {
+        id: 2,
+        image: book2,
+        price: '15',
+        title: 'Inferno',
+        description: "This book 'Inferno' follows his usual writing style, format, and layout. His stories are always well-written and clearly, a lot of research goes into them. This book certainly raises some complex and difficult issues around mankind's growing population and the earth's limited resources, and all of this get's entwined in religion, symbols, Italian history, and the works of Dante."
+      },
+      {
+        id: 3,
+        image: book3,
+        price: '29.99',
+        title: 'The Lost Symbol',
+        description: 'The Lost Symbol is a masterstroke of storytelling--a deadly race through a real-world labyrinth of codes, secrets, and unseen truths . . . all under the watchful eye of Brown’s most terrifying villain to date. Set within the hidden chambers, tunnels, and temples of Washington, D.C.,'
+      }
+    ]
+
+export default data
